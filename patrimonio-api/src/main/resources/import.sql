4c740b0f557053ac265342e0e05f615f5073c5c8
trickpasp <patrickeeep12@gmail.com>
2017-10-12 07:25:26 -0300

    adicionando import.sql

---
+++ patrimonio-api/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into item (etiqueta, descricao, data_aquisicao) values ('NTO123', 'Notebook i7', sysdate());
+insert into item (etiqueta, descricao, data_aquisicao) values ('AU9875', 'Ford Fiesta', sysdate());
+
