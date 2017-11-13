f47f80a386afd78fc53c047141c975922d0a01b5
Patrick da Silva <patrickeeep12@gmail.com>
2017-11-13 13:21:56 -0300

    adicionando o restante do curso

--- patrimonio-ui/src/app/app.module.ts
+++ patrimonio-ui/src/app/app.module.ts
@@ -1,18 +1,39 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { DataTableModule, ButtonModule, InputTextModule,
+  InputMaskModule, CalendarModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+import { ItemService } from './item/item.service';
+import { FormControl } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ItemCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    DataTableModule,
+    ButtonModule,
+    InputTextModule,
+    InputMaskModule,
+    CalendarModule
   ],
-  providers: [],
+  providers: [
+    ItemService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
